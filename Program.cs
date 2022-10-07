d16220b25a376ccaa201e36877b75e93746f30df
rodrigogrigo <rodrigogrigoleto@gmail.com>
2022-10-06 21:12:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ADO.NET Puro para conexão ao banco
+
+        const string connectionString 
+            = "Server=localhost,1433;Database=NOME_DATABASE;User ID=ID_CONEXAO;Password=PASSWORD;Trusted_Connection=False; TrustServerCertificate=True;";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            Console.WriteLine("Conectado");
+            connection.Open();
+
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                }
+            }
+        }
+    }
+}
